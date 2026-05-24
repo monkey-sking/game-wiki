971a88179d1e958fc30b47c01a7beb0420bc614b
runking <monkey-sking@users.noreply.github.com>
2026-05-24 09:55:32 +0800

    Initial setup: Quartz + Game Wiki structure

---
+++ quartz.config.ts
@@ -0,0 +1,97 @@
+import { QuartzConfig } from "./quartz/cfg"
+import * as Plugin from "./quartz/plugins"
+
+/**
+ * Quartz 4 Configuration
+ *
+ * See https://quartz.jzhao.xyz/configuration for more information.
+ */
+const config: QuartzConfig = {
+  configuration: {
+    pageTitle: "Game Wiki",
+    pageTitleSuffix: " - 游戏知识库",
+    enableSPA: true,
+    enablePopovers: true,
+    analytics: {
+      provider: "plausible",
+    },
+    locale: "zh-CN",
+    baseUrl: "game.7caifei.com",
+    ignorePatterns: ["private", "templates", ".obsidian"],
+    defaultDateType: "modified",
+    theme: {
+      fontOrigin: "googleFonts",
+      cdnCaching: true,
+      typography: {
+        header: "Schibsted Grotesk",
+        body: "Source Sans Pro",
+        code: "IBM Plex Mono",
+      },
+      colors: {
+        lightMode: {
+          light: "#f8f6f0",
+          lightgray: "#e8e4da",
+          gray: "#a09880",
+          darkgray: "#4a3f30",
+          dark: "#2d2418",
+          secondary: "#6b4c8a",
+          tertiary: "#c9a84c",
+          highlight: "rgba(107, 76, 138, 0.10)",
+          textHighlight: "#c9a84c44",
+        },
+        darkMode: {
+          light: "#1a1820",
+          lightgray: "#2a2835",
+          gray: "#5a5670",
+          darkgray: "#c8c4d0",
+          dark: "#e8e4f0",
+          secondary: "#9b7cba",
+          tertiary: "#d4b85c",
+          highlight: "rgba(155, 124, 186, 0.15)",
+          textHighlight: "#d4b85c44",
+        },
+      },
+    },
+  },
+  plugins: {
+    transformers: [
+      Plugin.FrontMatter(),
+      Plugin.CreatedModifiedDate({
+        priority: ["frontmatter", "git", "filesystem"],
+      }),
+      Plugin.SyntaxHighlighting({
+        theme: {
+          light: "github-light",
+          dark: "github-dark",
+        },
+        keepBackground: false,
+      }),
+      Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
+      Plugin.GitHubFlavoredMarkdown(),
+      Plugin.TableOfContents(),
+      Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
+      Plugin.Description(),
+      Plugin.Latex({ renderEngine: "katex" }),
+    ],
+    filters: [Plugin.RemoveDrafts()],
+    emitters: [
+      Plugin.AliasRedirects(),
+      Plugin.ComponentResources(),
+      Plugin.ContentPage(),
+      Plugin.FolderPage(),
+      Plugin.TagPage(),
+      Plugin.ContentIndex({
+        enableSiteMap: true,
+        enableRSS: true,
+      }),
+      Plugin.Assets(),
+      Plugin.Static(),
+      Plugin.Favicon(),
+      Plugin.NotFoundPage(),
+      // Comment out CustomOgImages to speed up build time
+      Plugin.CustomOgImages(),
+    ],
+  },
+}
+
+export default config
